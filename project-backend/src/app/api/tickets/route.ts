178d9c11f815a4f19a960f6a8c121648ce4501d4
Potaeeatsthis <whoareyou.top@gmail.com>
2026-01-29 00:51:57 +0700

    FEAT: Remove title as an input

--- project-backend/src/app/api/tickets/route.ts
+++ project-backend/src/app/api/tickets/route.ts
@@ -60,11 +60,9 @@ export async function POST(request: Request) {
       .from('tickets')
       .insert({
         description: message,
-	title: title || null,
         status: 'DRAFT',
         created_by: userId || null,
 	user_email: email,
-	origin: 'web',
       })
       .select()
       .single();
